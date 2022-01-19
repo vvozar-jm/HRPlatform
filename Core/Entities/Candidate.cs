936f86409ff61fdaf4629254aaf3520789d83d0f
Vladimir Vozar <vvozar@jminsure.com>
2022-01-19 20:44:50 +0100

    Added entities

---
+++ Core/Entities/Candidate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core.Entities
+{
+    public class Candidate : BaseEntity
+    {
+        public Candidate()
+        {
+            Skills = new List<Skill>();
+        }
+
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string ContactNumber { get; set; }
+        public string Email { get; set; }
+        public IList<Skill> Skills { get; set; }
+    }
+}
---
+++ Core/Entities/Skill.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Core.Entities
+{
+    public class Skill : BaseEntity
+    {
+        public Skill()
+        {
+            Candidates = new List<Candidate>();
+        }
+
+        public string Name { get; set; }
+        public IList<Candidate> Candidates { get; set; }
+    }
+}
