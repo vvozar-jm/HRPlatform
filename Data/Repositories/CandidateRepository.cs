569fa59c556d60309a16ecf487d1b0d7fbd23ddf
Vladimir Vozar <vvozar@jminsure.com>
2022-01-19 22:30:14 +0100

    Added repositories

---
+++ Data/Repositories/CandidateRepository.cs
@@ -0,0 +1,28 @@
+﻿using Core.Entities;
+using Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Data.Repositories
+{
+    public class CandidateRepository : ICandidateRepository
+    {
+        private readonly HrPlatformDbContext _context;
+
+        public CandidateRepository(HrPlatformDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Candidate> GetCandidateById(int id)
+        {
+            return await _context.Candidates.FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<IList<Candidate>> GetCandidates()
+        {
+            return await _context.Candidates.ToListAsync();
+        }
+    }
+}
---
+++ Data/Repositories/SkillRepository.cs
@@ -0,0 +1,28 @@
+﻿using Core.Entities;
+using Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Data.Repositories
+{
+    public class SkillRepository : ISkillRepository
+    {
+        private readonly HrPlatformDbContext _context;
+
+        public SkillRepository(HrPlatformDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Skill> GetSkillById(int id)
+        {
+            return await _context.Skills.FirstOrDefaultAsync(s => s.Id == id);
+        }
+
+        public async Task<IList<Skill>> GetSkills()
+        {
+            return await _context.Skills.ToListAsync();
+        }
+    }
+}
